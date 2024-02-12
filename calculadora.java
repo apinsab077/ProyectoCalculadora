af0530bfa837820864e20bc7d82986f072309a86
apinsab077 <156417575+apinsab077@users.noreply.github.com>
2024-02-12 14:20:19 +0100

    Add files via upload

---
+++ calculadora.java
@@ -0,0 +1,29 @@
+package calculadora;
+
+//Clase Calculadora
+class Calculadora {
+ private int numero1;
+ private int numero2;
+
+ // Constructor
+ public Calculadora(int numero1, int numero2) {
+     this.numero1 = numero1;
+     this.numero2 = numero2;
+ }
+
+ // Método para realizar operación
+ public int realizarOperacion(String operacion) {
+     switch (operacion) {
+         case "suma":
+             return suma();
+         default:
+             System.out.println("Operación no válida");
+             return 0;
+     }
+ }
+
+ // Método suma
+ public int suma() {
+     return numero1 + numero2;
+ }
+}
